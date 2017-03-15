6a2577597300154f6be06b83657cd317dcc75d6d
Egor Volkov <egoralvolk@gmail.com>
2017-03-16 01:00:50 +0700

    Skeleton. Fixed bugs #2.

--- wood_site/urls.py
+++ wood_site/urls.py
@@ -17,6 +17,6 @@ from django.conf.urls import url, include
 from django.contrib import admin
 
 urlpatterns = [
-    url(r'^plants/', include('wood.urls')),
+    url(r'^wood/', include('wood.urls')),
     url(r'^admin/', admin.site.urls),
 ]
