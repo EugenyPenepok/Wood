ee024065028e2d856d106ea7703fb0feedd44396
eliseev-eag <eliseev-eag@yandex.ru>
2017-03-19 18:31:46 +0700

    Добавил ссылку на категорию

--- wood/views.py
+++ wood/views.py
@@ -1,7 +1,15 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
-
 # Create your views here.
+from wood.models import Category
+
+
 def get_values(_request):
     return render(_request, 'index.html')
+
+
+def get_category(_request):
+    categories = Category.objects.all()
+    context = {"category": categories}
+    return render(_request, "category.html", context)
