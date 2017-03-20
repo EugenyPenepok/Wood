9cfa7161d98a49efba0519222ff335f5f145bb5b
eliseev-eag <eliseev-eag@yandex.ru>
2017-03-20 10:35:37 +0700

    Реализовал CRUD для категорий

--- wood/views.py
+++ wood/views.py
@@ -1,6 +1,6 @@
-from django.http import HttpResponse
 from django.shortcuts import render, redirect
-
+from django.core.files.storage import default_storage
+from django.core.files.base import ContentFile
 # Create your views here.
 from wood.models import *
 
@@ -41,12 +41,32 @@ def category_delete(_request, category_id):
 
 
 def category_add(request):
-    return render(request, 'addcategory.html')
+    if request.method == 'GET':
+        return render(request, 'addcategory.html')
+    elif request.method == 'POST':
+        image_file = (request.FILES['image'])
+        path_prefix = 'wood/static/'
+        save_path = "upload/category/" + image_file.name
+        upload_path = default_storage.save(path_prefix + save_path, ContentFile(image_file.read()))
+        real_path = upload_path.split(path_prefix)[1]
+        category = Category(name=request.POST['name'],
+                            description=request.POST['comment'],
+                            image_path=real_path)
+        category.save()
+        return redirect('category')
 
 
 def category_edit(request, category_id):
     category = Category.objects.get(pk=category_id)
     if request.method == 'POST':
+        image_file = request.FILES.get('image', False)
+        if image_file:
+            image_file = (request.FILES['image'])
+            path_prefix = 'wood/static/'
+            save_path = "upload/category/" + image_file.name
+            upload_path = default_storage.save(path_prefix + save_path, ContentFile(image_file.read()))
+            real_path = upload_path.split(path_prefix)[1]
+            category.image_path = real_path
         category.name = request.POST['name']
         category.description = request.POST['comment']
         category.save()
