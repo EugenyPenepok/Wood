1cf52ffeb9dc2e13b9901fb669403f649b0756fe
EugenyPenepok <eugenyt0@gmail.com>
2017-03-21 16:15:06 +0700

    Форма регистрации и исправленные шрифты

--- wood/urls.py
+++ wood/urls.py
@@ -5,6 +5,7 @@ from . import views
 urlpatterns = [
     url(r'^$', views.get_values, name='index'),
     url(r'^order$', views.get_orders, name='order'),
+    url(r'^register$', views.get_reg, name='register'),
     url(r'^category$', views.get_category, name='category'),
     url(r'^category/(?P<category_id>[0-9]+)/delete', views.category_delete, name='category_delete'),
     url(r'^category/add$', views.category_add, name='category_add'),
