d9eade3588b6ffe5f30d4cf5184ef2cfc4c0add7
Egor Volkov <egoralvolk@gmail.com>
2017-03-22 19:52:08 +0700

    Восстановление

--- wood/static/js/ajax_update_product.js
+++ wood/static/js/ajax_update_product.js
@@ -1,12 +1,23 @@
-/**
- * Created by admin on 22.03.2017.
- */
 $(function () {
 
-    var update_info = function() {
-        alert('Работает!');
+    var update_product = function() {
+        var select = $(this);
+        $.ajax({
+            url: select.attr("data-url"),
+            type: "get",
+            data: {"name_material": select.val()},
+            dataType: 'json',
+            cache: false,
+            success: function(data){
+                if(data.none){  // смотрим ответ от сервера и выполняем соответствующее действие
+                    alert("Нет изделий с такими параметрами.");
+                }else{
+                    //$("#plants-blog").html(data.html_plants_blog);
+                    alert("Есть изделия.");
+                }
+            }
+        });
     };
 
-    $("#material_selector").on("click", update_info);
-
+    $("#material_selector").on("change", update_product);
 });
