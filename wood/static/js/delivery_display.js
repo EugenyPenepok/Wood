f7559b83eba9837336228824b494187b5f4a200d
eliseev-eag <eliseev-eag@yandex.ru>
2017-04-05 12:10:14 +0700

    fix для модальных окон редактирования заказа и долгожданные крестики

--- wood/static/js/delivery_display.js
+++ wood/static/js/delivery_display.js
@@ -11,10 +11,26 @@ function validate() {
         document.getElementById("delivery_address").style.display = "none";
     }
 }
+function validate_personal() {
+    if (document.getElementById('isDelivered_personal').checked) {
+        document.getElementById("delivery_label_personal").style.display = "initial";
+        document.getElementById("delivery_address_personal").style.display = "initial";
+
+    } else {
+        document.getElementById("delivery_label_personal").style.display = "none";
+        document.getElementById("delivery_address_personal").style.display = "none";
+    }
+}
 $(document).ready(function () {
     $(".edit_order").click(function () {
             var order_id = $(this).data('order_id');
             $("#order_id").val(order_id);
         }
     );
+    $(".edit_personal_order").click(function () {
+            var order_id = $(this).data('order_id');
+            $("#personal_order_id").val(order_id);
+        }
+    );
+
 });
